58a5e3c7a92c5cddd197992dcbaa9e1f8b4de5c2
Aytech <ayokunumitoluwani00@gmail.com>
2022-07-24 18:22:49 +0100

    new commit

--- js/main.js
+++ js/main.js
@@ -56,38 +56,32 @@ function plusSlides(n) {
 }
 
 
-$('.slider').slick({
-  dots: true,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 4,
-  slidesToScroll: 4,
+
+// Slick carousel
+
+
+$('.slide-inner-container').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
   responsive: [
     {
-      breakpoint: 1024,
+      breakpoint: 768,
       settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
       }
     },
     {
       breakpoint: 480,
       settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
       }
     }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
   ]
 });
