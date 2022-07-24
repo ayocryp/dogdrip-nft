3c97bedc76902cd880e902b604ffebabe7aae368
your_username <ayodejifasesin4505@gmail.com>
2022-07-24 10:15:11 -0500

    roadmap fix

--- js/main.js
+++ js/main.js
@@ -16,33 +16,78 @@ setInterval(function () {
 }, 400);
 
 // Countdown
-let countDown = new Date("Oct 2, 2022 00:00:00").getTime();
-let launchDate = setInterval(function () {
-  // current date
-  let currentDate = new Date().getTime();
+// let countDown = new Date("Oct 2, 2022 00:00:00").getTime();
+// let launchDate = setInterval(function () {
+//   // current date
+//   let currentDate = new Date().getTime();
 
-  // Time difference between current date and launch date
-  let timeDifference = countDown - currentDate;
+//   // Time difference between current date and launch date
+//   let timeDifference = countDown - currentDate;
 
-  // Days, Hours, Minuntes and Sec calculation
-  let days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-  let hours = Math.floor(
-    (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
-  let minuntes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+//   // Days, Hours, Minuntes and Sec calculation
+//   let days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+//   let hours = Math.floor(
+//     (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+//   );
+//   let minuntes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+//   let seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
-  // display to the DOM
-  document.querySelector("#days").innerText = `${days}`;
-  document.querySelector("#hours").innerText = `${hours}`;
-  document.querySelector("#minutes").innerText = `${minuntes}`;
-  document.querySelector("#sec").innerText = `${seconds}`;
+//   // display to the DOM
+//   document.querySelector("#days").innerText = `${days}`;
+//   document.querySelector("#hours").innerText = `${hours}`;
+//   document.querySelector("#minutes").innerText = `${minuntes}`;
+//   document.querySelector("#sec").innerText = `${seconds}`;
 
-  if (timeDifference < 0) {
-    clearInterval(launchDate);
-    document.querySelector("#days").innerText = `00`;
-    document.querySelector("#hours").innerText = `00`;
-    document.querySelector("#minutes").innerText = `00`;
-    document.querySelector("#sec").innerText = `00`;
-  }
-}, 1000);
+//   if (timeDifference < 0) {
+//     clearInterval(launchDate);
+//     document.querySelector("#days").innerText = `00`;
+//     document.querySelector("#hours").innerText = `00`;
+//     document.querySelector("#minutes").innerText = `00`;
+//     document.querySelector("#sec").innerText = `00`;
+//   }
+// }, 1000);
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+
+$('.slider').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
