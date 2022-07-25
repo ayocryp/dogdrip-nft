7381612c2b320c2f169c4737789dd656ec02e96b
your_username <ayodejifasesin4505@gmail.com>
2022-07-24 21:44:18 -0500

    added countdown for desktop

--- js/main.js
+++ js/main.js
@@ -16,53 +16,50 @@ setInterval(function () {
 }, 400);
 
 // Countdown
-// let countDown = new Date("Oct 2, 2022 00:00:00").getTime();
-// let launchDate = setInterval(function () {
-//   // current date
-//   let currentDate = new Date().getTime();
+let countDown = new Date("Oct 2, 2022 00:00:00").getTime();
+let launchDate = setInterval(function () {
+  // current date
+  let currentDate = new Date().getTime();
 
-//   // Time difference between current date and launch date
-//   let timeDifference = countDown - currentDate;
+  // Time difference between current date and launch date
+  let timeDifference = countDown - currentDate;
 
-//   // Days, Hours, Minuntes and Sec calculation
-//   let days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-//   let hours = Math.floor(
-//     (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-//   );
-//   let minuntes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-//   let seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+  // Days, Hours, Minuntes and Sec calculation
+  let days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  let hours = Math.floor(
+    (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  let minuntes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
-//   // display to the DOM
-//   document.querySelector("#days").innerText = `${days}`;
-//   document.querySelector("#hours").innerText = `${hours}`;
-//   document.querySelector("#minutes").innerText = `${minuntes}`;
-//   document.querySelector("#sec").innerText = `${seconds}`;
+  // display to the DOM
+  document.querySelector("#days").innerText = `${days}`;
+  document.querySelector("#hours").innerText = `${hours}`;
+  document.querySelector("#minutes").innerText = `${minuntes}`;
+  document.querySelector("#sec").innerText = `${seconds}`;
 
-//   if (timeDifference < 0) {
-//     clearInterval(launchDate);
-//     document.querySelector("#days").innerText = `00`;
-//     document.querySelector("#hours").innerText = `00`;
-//     document.querySelector("#minutes").innerText = `00`;
-//     document.querySelector("#sec").innerText = `00`;
-//   }
-// }, 1000);
+  if (timeDifference < 0) {
+    clearInterval(launchDate);
+    document.querySelector("#days").innerText = `00`;
+    document.querySelector("#hours").innerText = `00`;
+    document.querySelector("#minutes").innerText = `00`;
+    document.querySelector("#sec").innerText = `00`;
+  }
+}, 1000);
 
 let slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
-
-
 
 // Slick carousel
 
-
-$('.slide-inner-container').slick({
+$(".slide-inner-container").slick({
   centerMode: true,
-  centerPadding: '60px',
+  centerPadding: "60px",
   slidesToShow: 3,
   responsive: [
     {
@@ -70,18 +67,18 @@ $('.slide-inner-container').slick({
       settings: {
         arrows: false,
         centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
-      }
+        centerPadding: "40px",
+        slidesToShow: 3,
+      },
     },
     {
       breakpoint: 480,
       settings: {
         arrows: false,
         centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ]
+        centerPadding: "40px",
+        slidesToShow: 1,
+      },
+    },
+  ],
 });
