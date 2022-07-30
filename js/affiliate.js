7febd3eda96cbeab4b465e67b1d5972a95060109
Aytech <ayokunumitoluwani00@gmail.com>
2022-07-30 02:31:05 +0100

    new commit

---
+++ js/affiliate.js
@@ -0,0 +1,42 @@
+
+// First name 
+
+const firstNameError = document.getElementById('first-name-error');
+const lastNameError = document.getElementById('last-name-error');
+const emailError = document.getElementById('email-error');
+const passwordError = document.getElementById('password-error');
+
+function validateFirstName(){
+  let firstName = document.getElementById('first-name').value;
+  if(firstName.length == 0){
+    
+    firsterror.innerHTML = 'First name is required'
+    
+  } else{
+    
+  }
+}
+
+
+
+// Last name
+
+
+
+
+
+
+
+
+
+// Email
+
+
+
+
+
+
+
+
+
+// Password
