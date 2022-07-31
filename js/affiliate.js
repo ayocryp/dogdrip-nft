8f1776f4341687a067850bd7f00707a3e392b1af
Aytech <ayokunumitoluwani00@gmail.com>
2022-07-31 17:44:09 +0100

    new commit

--- js/affiliate.js
+++ js/affiliate.js
@@ -5,7 +5,7 @@ const lastNameError = document.getElementById("last-name-error");
 const emailError = document.getElementById("email-error");
 const passwordError = document.getElementById("password-error");
 
-function validate() {
+function validateFirstName() {
   let firstName = document.getElementById("first-name").value;
   if (firstName.length == 0) {
     firsterror.innerHTML = "First name is required";
@@ -13,24 +13,39 @@ function validate() {
     firsterror.innerHTML = "";
   }
 
-  let lastName = document.getElementById("last-name").value;
-  if (lastName.length == 0) {
-    lastNameError.innerHTML = "Last name required";
-  } else {
-    lastNameError.innerHTML = "";
-  }
+}
 
-  let emailValidate = document.getElementById("email").value;
-  if (emailValidate.length == 0) {
-    emailError.innerHTML = "Email is required";
-  } else {
-    emailError.innerHTML = "";
-  }
 
-//   let passwordValidate = document.getElementById("password").value;
-//   if (passwordValidate.length === 0) {
-//     passwordError.innerHTML = " A password is required";
-//   } else {
-//     passwordError.innerHTML = "";
-//   }
+
+function validateLastName() {
+
+    let lastName = document.getElementById("last-name").value;
+    if (lastName.length == 0) {
+      lastNameError.innerHTML = "Last name required";
+    } else {
+      lastNameError.innerHTML = "";
+    }
+    
+}
+
+
+
+function validateEmail(){
+    let emailValidate = document.getElementById("email").value;
+    if (emailValidate.length == 0) {
+      emailError.innerHTML = "Email is required";
+    } else {
+      emailError.innerHTML = "";
+    }
+}
+
+
+function validatePassowrd() {
+      let passwordValidate = document.getElementById("password").value;
+  if (passwordValidate.length === 0) {
+    passwordError.innerHTML = " A password is required";
+  } else {
+    passwordError.innerHTML = "";
+  }
+    
 }
