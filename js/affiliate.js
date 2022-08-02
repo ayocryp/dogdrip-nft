436c4e4e7e5bf772e45fb1e4f7bb9aebb3c635ba
Aytech <ayokunumitoluwani00@gmail.com>
2022-08-02 15:11:23 +0100

    new commit

--- js/affiliate.js
+++ js/affiliate.js
@@ -1,9 +1,10 @@
 // Form validation
 
-const firstNameError = document.getElementById("firsterror");
-const lastNameError = document.getElementById("last-name-error");
-const emailError = document.getElementById("email-error");
-const passwordError = document.getElementById("password-error");
+const firstNameError = document.getElementById("firsterror")
+const lastNameError = document.getElementById("last-name-error")
+const emailError = document.getElementById("email-error")
+const passwordError = document.getElementById("password-error")
+const submitError = document.getElementById('submit-error')
 
 function validateFirstName() {
   let firstName = document.getElementById("first-name").value;
@@ -40,12 +41,26 @@ function validateEmail(){
 }
 
 
-function validatePassowrd() {
+function validatePassword() {
       let passwordValidate = document.getElementById("password").value;
   if (passwordValidate.length == 0) {
     passwordError.innerHTML = " A password is required";
   } else {
     passwordError.innerHTML = "";
   }
+
+  if ( passwordValidate.length < 8){
+    passwordError.innerHTML = "Password must 8 characters";
+  }
+
+
     
 }
+function validateButton(){
+  if(!validateFirstName() || !validateLastName() || !validateEmail() || !validatePassword ){
+    // setTimeout.style.display = "block"
+    submitError.innerHTML = 'Please fix error to continue';
+    setTimeout(function(){submitError.style.display = 'none'}, 3000)
+  }
+}
+
