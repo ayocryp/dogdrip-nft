6c1a433dac32c074e87d3cf5c1726696073d0364
Aytech <ayokunumitoluwani00@gmail.com>
2022-08-05 01:52:11 +0100

    new commit

--- js/controlbtn.js
+++ js/controlbtn.js
@@ -1,33 +1,33 @@
-// $('.main-slider-container').slick({
-//     dots: false,
-//     infinite: false,
-//     speed: 1000,
-//     slidesToShow: 3,
-//     slidesToScroll: 1,
-//     responsive: [
-//       {
-//         breakpoint: 1024,
-//         settings: {
-//           slidesToShow: 2,
-//           slidesToScroll: 2,
-//           infinite: true,
-//           dots: true
-//         }
-//       },
-//       {
-//         breakpoint: 600,
-//         settings: {
-//           slidesToShow: 1,
-//           slidesToScroll: 1
-//         }
-//       },
-//       {
-//         breakpoint: 480,
-//         settings: {
-//           slidesToShow: 1,
-//           slidesToScroll: 1
+$('.nft-gallery-container').slick({
+    dots: false,
+    infinite: false,
+    speed: 1000,
+    slidesToShow: 4,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
         
-//         }
-//       }
-//     ]
-//   });
+        }
+      }
+    ]
+  });
